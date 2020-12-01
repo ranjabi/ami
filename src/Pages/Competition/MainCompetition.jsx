dce13f6719cfd79c790c4841bb20ec0dfb02bc37
pege <mpg066@gmail.com>
2020-12-01 16:46:52 +0700

    adding download pdf button

--- src/Pages/Competition/MainCompetition.jsx
+++ src/Pages/Competition/MainCompetition.jsx
@@ -2,26 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Layout } from "../Layout/Layout";
 import { competitionRoutes } from "../../routes/routes.js";
+import { Button } from "../../components/Form/Form";
 import "./MainCompetition.scss";
 
 export const MainCompetition = () => {
+
+  const pdfLink =
+		"https://drive.google.com/file/d/1HbKnCx6Ux0uuTF96xRUL2tollPpNwip1/view?usp=sharing";
+
+  const downloadPDF = () => {
+		window.open(pdfLink, "_blank");
+	};
+
   return (
-    <Layout>
-      <div className="mainCompetition-container">
-        <h1>Lomba</h1>
-        <div className="competitions-container">
-          {competitionRoutes.map((competition) => (
-            <NavLink
-              key={competition.label}
-              to={competition.path}
-              className="competition-thumbnail"
-            >
-              <img src={competition.image} alt={competition.label} />
-              <p>{competition.label}</p>
-            </NavLink>
-          ))}
-        </div>
-      </div>
-    </Layout>
-  );
+		<Layout>
+			<div className='mainCompetition-container'>
+				<h1>Lomba</h1>
+				<div className='competitions-container'>
+					{competitionRoutes.map((competition) => (
+						<NavLink
+							key={competition.label}
+							to={competition.path}
+							className='competition-thumbnail'
+						>
+							<img src={competition.image} alt={competition.label} />
+							<p>{competition.label}</p>
+						</NavLink>
+					))}
+				</div>
+				<Button onClick={() => downloadPDF()} onClickParams='' text="Download PDF Lomba" />
+			</div>
+		</Layout>
+	);
 };
