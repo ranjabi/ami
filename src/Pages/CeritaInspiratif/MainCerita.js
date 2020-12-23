57a6791ef2ed84769f6c0bb67b8ba8d2c3653414
pege <mpg066@gmail.com>
2020-12-23 15:55:25 +0700

    adding prev and next button as carousel

--- src/Pages/CeritaInspiratif/MainCerita.js
+++ src/Pages/CeritaInspiratif/MainCerita.js
@@ -3,6 +3,7 @@ import { Layout } from "../Layout/Layout";
 import { Dropdown } from "../../components/Form/Form";
 import data from "./data.json";
 import ReactReadMoreReadLess from "react-read-more-read-less";
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 import "./MainCerita.scss";
 
 export const MainCerita = () => {
@@ -13,6 +14,11 @@ export const MainCerita = () => {
         setCeritaId(value);
     }
 
+    const handleCarousel = (change) => {
+			let target = ceritaId + change;
+			target = target < 0 ? target + data.length : target % data.length;
+			setCeritaId(target);
+		};
 
     const [headerScrolled, setHeaderScrolled] = useState(false);
     const [navbarOffset, setNavbarOffset] = useState(0);
@@ -38,45 +44,69 @@ export const MainCerita = () => {
     }, [])
 
     return (
-        <Layout>
-            <div style={{minHeight: "calc(100vh - "+ navbarOffset +"px)"}} className="mainCerita-container">
-                <div className={"headerCerita " + (headerScrolled? "headerScrolled" : "")}>
-                    <Dropdown
-                        useKey
-                        hideTooltip
-                        className="ddChangeCerita"
-                        value={ceritaId}
-                        options={data.map((d)=>{
-                            return "Cerita Perjuangan " + d.author;
-                        })}
-                        handleChange={changeCerita}>
-                    </Dropdown>
-                </div>
-                <div style={{opacity: 0, display: (headerScrolled ? "block" : "none")}} className="headerCerita">
-                    <Dropdown
-                        useKey
-                        hideTooltip
-                        className="ddChangeCerita"
-                        value={ceritaId}
-                        options={[]}>
-                    </Dropdown>
-                </div>
-                <div className="bodyCerita">
-                    <ReactReadMoreReadLess
-                        charLimit={400}
-                        readMoreText={"Read more ▼"}
-                        readLessText={"Read less ▲"}
-                    >
-                        {data[ceritaId].cerita}
-                    </ReactReadMoreReadLess>
-                </div>
-                <div className={"footerCerita"}>
-                    <div className="footerContainer">
-                        <p>{data[ceritaId].author}</p>
-                        <p>{data[ceritaId].nim+" / "+data[ceritaId].fakultas+" / "+data[ceritaId].jurusan}</p>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    );
+			<Layout>
+				<div
+					style={{minHeight: "calc(100vh - " + navbarOffset + "px)"}}
+					className='mainCerita-container'
+				>
+					<div
+						className={
+							"headerCerita " + (headerScrolled ? "headerScrolled" : "")
+						}
+					>
+						<Dropdown
+							useKey
+							hideTooltip
+							className='ddChangeCerita'
+							value={ceritaId}
+							options={data.map((d) => {
+								return "Cerita Perjuangan " + d.author;
+							})}
+							handleChange={changeCerita}
+						></Dropdown>
+					</div>
+					<div
+						style={{opacity: 0, display: headerScrolled ? "block" : "none"}}
+						className='headerCerita'
+					>
+						<Dropdown
+							useKey
+							hideTooltip
+							className='ddChangeCerita'
+							value={ceritaId}
+							options={[]}
+						></Dropdown>
+					</div>
+					<div className='bodyCerita'>
+						<FaChevronLeft
+							className='prev'
+							onClick={() => handleCarousel(-1)}
+						/>
+						<ReactReadMoreReadLess
+							charLimit={400}
+							readMoreText={"Read more ▼"}
+							readLessText={"Read less ▲"}
+						>
+							{data[ceritaId].cerita}
+						</ReactReadMoreReadLess>
+						<FaChevronRight
+							className='next'
+							onClick={() => handleCarousel(1)}
+						/>
+					</div>
+					<div className={"footerCerita"}>
+						<div className='footerContainer'>
+							<p>{data[ceritaId].author}</p>
+							<p>
+								{data[ceritaId].nim +
+									" / " +
+									data[ceritaId].fakultas +
+									" / " +
+									data[ceritaId].jurusan}
+							</p>
+						</div>
+					</div>
+				</div>
+			</Layout>
+		);
 };
