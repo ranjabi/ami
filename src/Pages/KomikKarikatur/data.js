affb51c6b0930ae223792064575d98def804236f
Garin <garin.kra@gmail.com>
2021-01-29 13:04:03 +0700

    fix: thumbnail for eksibisi komik karikatur

--- src/Pages/KomikKarikatur/data.js
+++ src/Pages/KomikKarikatur/data.js
@@ -13,7 +13,7 @@ export const data = [
     judul: "PENDIDIKAN MENGUBAH KEGELAPAN MENJADI TERANG",
     uraian:
       "Yoga merupakan seorang anak yang tinggal di desa, ayah dan ibunya adalah seorang petani lulusan SD dengan penghasilan yang pas-pasan, yang membuat Yoga sangat memahami bagaimana susahnya kehidupan orang tuanya. Orang tua Yoga pernah berpesan padanya bahwa hanya pendidikan dan kerja keraslah yang hanya mampu mengubah kehidupan mereka. Berkat pesan dari orang tuanya, Yoga selalu bekerja keras dan semangat untuk belajar, sehingga ia menjadi anak yang berprestasi di sekolah dan masuk ke PTN yang ia impikan dengan beasiswa penuh.\n     Dijenjang pendidikan yang lebih tinggi, Yoga semakin ditempa untuk maju, kemampuan Yoga juga semakin terarah dan terampil, serta melatih kemampuan bermasyarakat Yoga. \n     Setelah lulus dengan cepat, Yogapun lekas pulang, serta membangun usaha bagi warga dan sekolah yang baik bagi anak-anak didesanya,  dan telah mengubah kehidupan di desanya.\n     Oia...saat ia menempuh studinya, ia bertemu dengan seorang wanita yang menjadi  isterinya, dan diberkati tiga orang anak. Usaha Yoga dalam menempuh pendidikan hingga ke perguruan tinggi telah mengubah dunia dan kehidupannya.\n\n      Dan sekian narasi singkat yang dapat saya sampaikan dari komik yang saya buat, berjudul \" PENDIDIKAN MENGUBAH KEGELAPAN MENJADI TERANG \". Saya berharap komik ini dapat memberikan semangat dan pesan bagaimana pentingnya pendidikan dalam kehidupan kita semua. Terima Kasih.",
-    file_poster: Img1,
+    file_poster: Img2,
     link: "https://drive.google.com/open?id=1htueRAezdHvj7dg1RyqoiVAcHJI1a_QX",
   },
   {
@@ -23,7 +23,7 @@ export const data = [
     judul: "KULIAH ATAU KERJA",
     uraian:
       "Roni adalah siswa kelas 12, ia sedang kebingungan memilih antara kuliah atau kerja. Ia pun bertemu dengan alumni dari sekolah nya, bernama Arda. Arda menjelaskan bahwa kuliah itu banyak sekali manfaatnya, dan bisa membimbing menuju kesuksesan.\n\nSetelah mendengar penjelasan tersebut, Roni memantapkan niatnya untuk berkuliah. Arda pun menawarkannya untuk daftar di universitas yang sama dengannya. Roni menerima penawaran tersebut dengan penuh semangat.",
-    file_poster: Img2,
+    file_poster: Img5,
     link: "https://drive.google.com/open?id=13jtx5hkyZ9dkfEryiBOLxz5vvz_ENVwk",
   },
   {
@@ -33,7 +33,7 @@ export const data = [
     judul: "Future Goals",
     uraian:
       "*Kelahiran Suzy🎀\nSuzy mom : Alhamdulillah birth of beauty\n*Perjalanan pendidikan Suzy🎀\nFoto oren berkerudung lilac : TK!\nFoto biru muda bertopi merah : SD!\nFoto merah bertopi biru tua : SMP!\nFoto biru tua bertopi abu² : SMA!\n*Hasil perjalanan pendidikan Suzy🎀\nSuzy Dad,Mom : Terimakasih sudah berjuang nak, now you're doctor. Congratulation!💜\n*Suzy now🎀\nSuzy : This is me! I'm Dr. Suzy Sp.B👩‍⚕️\n\nPesan : Pendidikan sangat penting, jika bersungguh sungguh menjalaninya Insyaallah cita² mu tergapai seperti Suzy🎀",
-    file_poster: Img3,
+    file_poster: Img1,
     link: "https://drive.google.com/open?id=1HguCbgVkqbxhlhlwWHIgs6afM4IStrKc",
   },
   {
@@ -43,7 +43,7 @@ export const data = [
     judul: "Di Mana Pun Itu yang Kau Tuju ...",
     uraian:
       "Dunia kita berkembang amat pesat, dan hanya dalam beberapa tahun ke depan akan ada lebih banyak pekerjaan yang dilakukan oleh mesin dan algoritma dibanding manusia. Jutaan bidang pekerjaan baru akan dan sedang terciptakan sebagai penggantinya. Maka tidak lagi bisa dipungkiri bahwa kemauan dan kemampuan untuk belajar terus-menerus menjadi suatu hal yang penting yang harus dimiliki seseorang untuk bertahan hidup di era modern ini.\n\nDengan berpendidikan tinggi, kamu dapat menjadi kunci kemajuan Indonesia. Itu yang akan membekalimu dengan pengetahuan dan keterampilan untuk karir dan masa depan di era yang sangat kompetitif ini. Tidak masalah kamu mau menjadi astronot, fotografer, pengacara, atau pun komposer. Percayalah: di mana pun itu yang kau tuju ... pendidikanlah yang akan mengantarmu.",
-    file_poster: Img4,
+    file_poster: Img3,
     link: "https://drive.google.com/open?id=11g25rGs0zbn8X1mojI88ubvZBWGWz_3c",
   },
   {
@@ -53,7 +53,7 @@ export const data = [
     judul: "Moralitas Generasi Milenial",
     uraian:
       '"Moralitas Generasi Milenial"\nDua laki-laki anak SMA sepulang sekolah tidak sengaja berpapasan di jalan. Anak SMA yang berpenampilan rapih memakai jaket berwarna merah layaknya anak baik menegur temannya yang berpenampilan berantakan, bertindik di telinganya, memakai gelang seperti layaknyanya anak nakal. Anak berpenampilan baik itu mengatakan bahwa pendidikan di Indonesia tidak maju-maju dikarenakan pemuda zaman sekarang tidak memiliki moral, perilaku, dan penampilan yang baik seperti temannya itu. Lalu anak yang berpenampilan nakal itu  mengelak perkataannya dan langsung marah, menghajar teman yang menegurnya tadi sampai terjatuh keras ke jalan dengan mengatakan bahwa justru orang yang sok berpendidikan seperti temannya itulah yang membuat pendidikan di Indonesia tidak maju-maju.\nDi tengah perkelahian itu ada seseorang  yang berteriak menghentikan dua anak muda tersebut, dua anak muda itupun terkejut karena melihat sosok yang datang melerai perkelahiannya itu adalah Tan Malaka. Beliaupun langsung memberikan petuah "Tujuan pendidikan itu untuk mempertaam KECERDASAN, memperkukuh KEMAUAN, serta memperhalua PERASAAN" dan beliau melanjutkan petuahnya bahwa moral an budipekerti yag baik itu merupakan buah dari suksesnya sebuh pendidikan.\nAkhirnya dua anak SMA tersebut patuh dan meresapi perkataan seorang pahlawan Indonesia yang bernama Tan Malaka, keduanyapun berjabat tangan saling memaafkan.\nPesan terakhir dari komik karikatur ini yaitu "Pendidikan adalah senjata paling ampuh untuk mengubah dunia" (Nelson Mandela).',
-    file_poster: Img5,
+    file_poster: Img4,
     link: "https://drive.google.com/open?id=1JPem7TPOuRpaREPRUWY8Ch7qIIGZjeMH",
   },
   {
