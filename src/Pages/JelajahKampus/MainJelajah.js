ff221e4bfb985d93767c36c710d65c53b0ddf6c8
M Syahrul Surya Putra <65522975+msyahrulsp@users.noreply.github.com>
2021-02-04 21:43:31 +0700

    added deskripsi

--- src/Pages/JelajahKampus/MainJelajah.js
+++ src/Pages/JelajahKampus/MainJelajah.js
@@ -9,12 +9,17 @@ import ITB from "../../images/itb1920.png";
 
 export const MainJelajah = () => {
     const ganeshaLink = "http://bit.ly/JKAMI2021";
-    const zoomLink = "http://bit.ly/zoom_jk_ami2021";
+    const liveLink = "http://bit.ly/zoom_jk_ami2021";
 
     return (
 		<Layout>
 			<div className='mainJelajah-container'>
 				<h1>Jelajah Kampus</h1>
+				<p>
+					Jelajah Kampus ITB bersama AMI 2021 di ITB Day 2021! Bersama tour guide kami, 
+					berkelilinglah mengunjungi gedung-gedung dan kompleks kampus ITB yang tersohor. 
+					Tour yang virtual namun tetap imersif ini dirancang khusus untuk kalian, para calon-calon mahasiswa ITB!
+				</p>
 				<div className='jelajah-container'>
 					<ButtonSection
 						text = "Kampus Ganesha"
@@ -28,9 +33,9 @@ export const MainJelajah = () => {
 						reverse
 					/>
 					<ButtonSection
-						text = "Zoom Explanation Room"
+						text = "Live Campus Tour"
 						image = {ITB}
-						link = {zoomLink}
+						link = {liveLink}
 					/>
 				</div>
 			</div>
